f2d206f4b51591d8389716f477219bb0364845e3
Viacheslav Vinichenko <fortunebetting@gmail.com>
2023-03-25 16:39:40 +0200

    add profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { Profile } from './Profile';
+import user from '../user.json';
 export const App = () => {
   return (
     <div
@@ -7,10 +9,16 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
